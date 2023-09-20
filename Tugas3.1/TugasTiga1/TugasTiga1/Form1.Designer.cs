01367f46de432b2042ec1227a7af1b50a64ba8c8
JessAlvaa <122151811+JessAlvaa@users.noreply.github.com>
2023-09-20 23:10:48 +0700

    Add files via upload

---
+++ Tugas3.1/TugasTiga1/TugasTiga1/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace TugasTiga1
+{
+    partial class frmTugasTiga1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNim = new System.Windows.Forms.Label();
+            this.lblNama = new System.Windows.Forms.Label();
+            this.lblKelas = new System.Windows.Forms.Label();
+            this.btnTampilkan = new System.Windows.Forms.Button();
+            this.btnKosongkan = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNim
+            // 
+            this.lblNim.AutoSize = true;
+            this.lblNim.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNim.Location = new System.Drawing.Point(12, 37);
+            this.lblNim.Name = "lblNim";
+            this.lblNim.Size = new System.Drawing.Size(84, 24);
+            this.lblNim.TabIndex = 0;
+            this.lblNim.Text = "NIM       :";
+            // 
+            // lblNama
+            // 
+            this.lblNama.AutoSize = true;
+            this.lblNama.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNama.Location = new System.Drawing.Point(12, 72);
+            this.lblNama.Name = "lblNama";
+            this.lblNama.Size = new System.Drawing.Size(90, 24);
+            this.lblNama.TabIndex = 0;
+            this.lblNama.Text = "Nama    : ";
+            // 
+            // lblKelas
+            // 
+            this.lblKelas.AutoSize = true;
+            this.lblKelas.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKelas.Location = new System.Drawing.Point(12, 106);
+            this.lblKelas.Name = "lblKelas";
+            this.lblKelas.Size = new System.Drawing.Size(86, 24);
+            this.lblKelas.TabIndex = 0;
+            this.lblKelas.Text = "Kelas     :";
+            // 
+            // btnTampilkan
+            // 
+            this.btnTampilkan.Location = new System.Drawing.Point(38, 163);
+            this.btnTampilkan.Name = "btnTampilkan";
+            this.btnTampilkan.Size = new System.Drawing.Size(142, 40);
+            this.btnTampilkan.TabIndex = 1;
+            this.btnTampilkan.Text = "Tampilkan";
+            this.btnTampilkan.UseVisualStyleBackColor = true;
+            this.btnTampilkan.Click += new System.EventHandler(this.btnTampilkan_Click);
+            // 
+            // btnKosongkan
+            // 
+            this.btnKosongkan.Location = new System.Drawing.Point(219, 163);
+            this.btnKosongkan.Name = "btnKosongkan";
+            this.btnKosongkan.Size = new System.Drawing.Size(142, 40);
+            this.btnKosongkan.TabIndex = 2;
+            this.btnKosongkan.Text = "Kosongkan";
+            this.btnKosongkan.UseVisualStyleBackColor = true;
+            this.btnKosongkan.Click += new System.EventHandler(this.btnKosongkan_Click);
+            // 
+            // frmTugasTiga1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 249);
+            this.Controls.Add(this.btnKosongkan);
+            this.Controls.Add(this.btnTampilkan);
+            this.Controls.Add(this.lblKelas);
+            this.Controls.Add(this.lblNama);
+            this.Controls.Add(this.lblNim);
+            this.Name = "frmTugasTiga1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tugas 3.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNim;
+        private System.Windows.Forms.Label lblNama;
+        private System.Windows.Forms.Label lblKelas;
+        private System.Windows.Forms.Button btnTampilkan;
+        private System.Windows.Forms.Button btnKosongkan;
+    }
+}
+
---
+++ Tugas3.1/TugasTiga1/TugasTiga1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TugasTiga1
+{
+    public partial class frmTugasTiga1 : Form
+    {
+        public frmTugasTiga1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTampilkan_Click(object sender, EventArgs e)
+        {
+            lblNim.Text = "NIM       : 22.11.5135 ";
+            lblNama.Text = "Nama    : Jessica Alva N ";
+            lblKelas.Text = "Kelas     : 22 IF 10 ";
+        }
+
+        private void btnKosongkan_Click(object sender, EventArgs e)
+        {
+            lblNim.Text = "NIM       :  ";
+            lblNama.Text = "Nama    : ";
+            lblKelas.Text = "Kelas     :  ";
+        }
+    }
+}
